980f691004e6b7cf6733ca41442bba367123ef74
eriosdev <ezequielrq@hotmail.com>
2024-04-16 20:23:09 +0200

    quitamos archivos del repositorio

--- index.js
+++ index.js
@@ -5,6 +5,71 @@
 var express = require('express');
 var app = express();
 
+
+//  ES la api para la entrada de la fecha
+app.get('/api/:date?', (req, res) => {
+
+  console.log (req.params.date);
+
+  let varibleEntrada;
+
+  const dateParam = req.params.date; // 'tu_fecha_aqui'; // Reemplaza esto con tu fecha
+  let date;
+  
+  // Intenta convertir la fecha en formato Unix a una fecha JavaScript
+  if (/^\d+$/.test(dateParam)) {
+      date = new Date(parseInt(dateParam));
+  } else {
+      // Intenta parsear la fecha como una cadena ISO 8601
+      date = new Date(dateParam);
+  }
+  
+  if (isNaN(date.getTime())) {
+      console.log('Fecha inválida');
+  } else {
+      console.log(`Fecha recibida: ${date.toISOString()}`);
+  }
+  
+
+  if (/^\d+$/.test(dateParam)) {
+    console.log ("es unix");
+    date = new Date(parseInt(dateParam));
+} else if (/^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(.\d{3})?(Z|([+-]\d{2}:\d{2}))?)?$/.test(dateParam)) {
+    // Intenta parsear la fecha como una cadena ISO 8601
+    console.log ("es iso 8601");
+    date = new Date(dateParam);
+} else {
+    console.log ("formato invalido");
+    date = null;
+}
+
+
+  if (req.params.date) {
+    // Attempt to parse the date
+    varibleEntrada = new Date(req.params.date);
+  } else {
+    // If date parameter is empty, use current time
+    varibleEntrada = new Date();
+  }
+
+  // Check if the date is valid
+  if (!isNaN(varibleEntrada.getTime())) {
+    // Valid date, construct response
+    res.json({
+      unix: varibleEntrada.getTime(),
+      utc: varibleEntrada.toUTCString()
+    });
+  } else {
+    // Invalid date, return error
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+
+
+
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -19,10 +84,12 @@ app.get("/", function (req, res) {
 });
 
 
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
 
 
 
